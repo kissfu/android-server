47067edd1f870ba1a2f6df454dad789d7bb44ae6
able <able2012@163.com>
2020-10-28 18:58:18 +0800

    Add: element number form multi elements result

--- uia1/src/main/java/com/testerkit/uia/handlers/find/FindEvent.java
+++ uia1/src/main/java/com/testerkit/uia/handlers/find/FindEvent.java
@@ -91,13 +91,55 @@ public abstract class FindEvent extends SafeRequestHandler {
         return nodes;
     }
 
+    protected List<NodeInfo> findNodes() {
+        List<NodeInfo> nodes = new ArrayList<NodeInfo>();
+        try {
+            long start = System.currentTimeMillis();
+            int counter = 0;
+            while (System.currentTimeMillis() - start < step.getScroll().getTimeout()) {
+                Logger.iFunc(FUNC, "find element times ", counter++);
+                ReflectionUtil.clearAccessibilityCache();
+                UIDumpInfo dump = XMLHierarchy.getDumpInfo();
+                nodes = MatcherManager.getInstance().isMatch(step, dump);
+                if (nodes.size() > 0) {
+                    break;
+                }
+                WatcherManager.getInstance().runTimes(1);
+                // TODO 如果没找到需要滑屏幕
+            }
+
+            if (nodes.size() == 0) {
+                this.result.setError("没有找到一个元素！！！");
+                Logger.iFunc(FUNC, this.result.getError());
+            } else if (nodes.size() > 1) {
+                this.result.setError("找到多个元素！！！" + GsonUtil.gsonString(nodes));
+                Logger.iFunc(FUNC, this.result.getError());
+            }
+            return nodes;
+        } catch (UIAException e) {
+            this.result.setError("查找元素的时候异常！！！" + e.getMessage());
+            Logger.error(this.result.getError(), e);
+        } finally {
+
+        }
+        return nodes;
+    }
+
     protected NodeInfo findNode() {
-        List<NodeInfo> nodes = this.findNodes(true);
-        if (nodes.size() != 1) {
+        List<NodeInfo> nodes = this.findNodes();
+        if (nodes.size() < 1) {
             return null;
         }
-        this.result.setValue(GsonUtil.gsonString(nodes.get(0)));
-        return nodes.get(0);
+        int number = step.getEleNum();
+        if (number <= 0) {
+            number = 1;
+        }
+        if (nodes.size() < number) {
+            Logger.iFunc(FUNC, String.format("选择第%s个元素，但是只有%s个！！！", number, nodes.size()));
+            return null;
+        }
+        this.result.setValue(GsonUtil.gsonString(nodes.get(number - 1)));
+        return nodes.get(number - 1);
     }
 
     protected abstract boolean executeFindEvent(IRequest request) throws Exception;
